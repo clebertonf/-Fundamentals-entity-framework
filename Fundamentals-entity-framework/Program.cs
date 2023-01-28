1a4cb1de9aff6b31efd62154a376cf525bbe6574
Cleberton Carvalho <clebertonfgc@gmail.com>
2023-01-27 22:43:44 -0300

    Start project

---
+++ Fundamentals-entity-framework/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fundamentals_entity_framework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
