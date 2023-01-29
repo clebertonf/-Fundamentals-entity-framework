1874a90e60e6ff478fedb18c220a1dc5f64b7b6f
Cleberton Carvalho <clebertonfgc@gmail.com>
2023-01-29 17:37:07 -0300

    Create models

---
+++ Fundamentals-entity-framework/Models/Tag.cs
@@ -0,0 +1,9 @@
+﻿namespace Fundamentals_entity_framework.Models
+{
+    public class Tag
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Slug { get; set; }
+    }
+}
