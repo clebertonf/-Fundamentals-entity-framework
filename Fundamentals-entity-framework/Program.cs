e13fb7a71f3be52c23c8b5a74445bb830c0baf57
Cleberton Carvalho <clebertonfgc@gmail.com>
2023-01-31 22:57:31 -0300

    Creating a simple example of adding a tag to the bank

--- Fundamentals-entity-framework/Program.cs
+++ Fundamentals-entity-framework/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Fundamentals_entity_framework.Data;
+using Fundamentals_entity_framework.Models;
 using System;
 
 namespace Fundamentals_entity_framework
@@ -9,7 +10,9 @@ namespace Fundamentals_entity_framework
         {
             using (var context = new BlogDataContext())
             {
-                
+                var tag = new Tag() { Name = "AspNet", Slug = "aspnet-core" };
+                context.Tags.Add(tag);
+                context.SaveChanges();
             }
         }
     }
