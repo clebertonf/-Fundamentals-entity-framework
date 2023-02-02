d614c03c5f531d46c5638464693f90992c81976b
Cleberton Carvalho <clebertonfgc@gmail.com>
2023-02-01 22:50:06 -0300

    Add basic update and Add

--- Fundamentals-entity-framework/Program.cs
+++ Fundamentals-entity-framework/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Fundamentals_entity_framework.Data;
 using Fundamentals_entity_framework.Models;
 using System;
+using System.Linq;
 
 namespace Fundamentals_entity_framework
 {
@@ -10,9 +11,19 @@ namespace Fundamentals_entity_framework
         {
             using (var context = new BlogDataContext())
             {
-                var tag = new Tag() { Name = "AspNet", Slug = "aspnet-core" };
-                context.Tags.Add(tag);
-                context.SaveChanges();
+                // Add
+                //var tag = new Tag() { Name = "AspNet", Slug = "aspnet-core" };
+                //context.Tags.Add(tag);
+                //context.SaveChanges();
+
+                // Update
+                //var tag = context.Tags.FirstOrDefault(x => x.Id == 1);
+                //tag.Slug = "aspnetcore";
+
+                //context.Update(tag);
+                //context.SaveChanges();
+
+
             }
         }
     }
