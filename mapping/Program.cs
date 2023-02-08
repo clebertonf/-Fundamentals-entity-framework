c5ab27ab8df02bbd015dc1ae088534c717c05051
Cleberton Carvalho <clebertonfgc@gmail.com>
2023-02-07 22:59:18 -0300

    Crate project mapping

---
+++ mapping/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace mapping
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
