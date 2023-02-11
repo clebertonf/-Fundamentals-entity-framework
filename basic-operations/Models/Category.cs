08aa55c081cc9c3a4c1cfb4531ab83daf82947ea
Cleberton Carvalho <clebertonfgc@gmail.com>
2023-02-10 21:47:36 -0300

    Add Table, Key and Identity

--- basic-operations/Models/Category.cs
+++ basic-operations/Models/Category.cs
@@ -1,7 +1,13 @@
-﻿namespace basic_operations.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace basic_operations.Models
 {
+    [Table("Category")]
     public class Category
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] // Deixa explicito que o banco irá gera a chave primaria Id
         public int Id { get; set; }
         public string Name { get; set; }
         public string Slug { get; set; }
