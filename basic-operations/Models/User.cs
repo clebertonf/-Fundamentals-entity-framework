e7b91c379fb195888b74c00524348188966a0fef
Cleberton Carvalho <clebertonfgc@gmail.com>
2023-02-10 22:18:36 -0300

    ForeignKey

--- basic-operations/Models/User.cs
+++ basic-operations/Models/User.cs
@@ -1,7 +1,13 @@
-﻿namespace basic_operations.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace basic_operations.Models
 {
+    [Table("User")]
     public class User
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
