4870fbbecb1e91608da460cd6de47defcb5ab078
Cleberton Carvalho <clebertonfgc@gmail.com>
2023-02-12 19:07:54 -0300

    List posts

--- basic-operations/Program.cs
+++ basic-operations/Program.cs
@@ -1,6 +1,8 @@
 ﻿using basic_operations.Data;
 using basic_operations.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
+using System.Linq;
 
 namespace basic_operations
 {
@@ -10,38 +12,49 @@ namespace basic_operations
         {
             // using (var context = new BlogDataContext()) { };
 
+            // insert com sub conjuntos
            using var context = new BlogDataContext();
 
-            var user = new User()
-            {
-                Name = "Cleberton",
-                Slug = "clebertoFrancisco",
-                Email = "cleberton@gmail.com",
-                Bio = "programador back-end",
-                Image = "https://www.instagram.com/cleberton_dev/",
-                PasswordHash = "123321558899"
-            };
+            //var user = new User()
+            //{
+            //    Name = "Cleberton",
+            //    Slug = "clebertoFrancisco",
+            //    Email = "cleberton@gmail.com",
+            //    Bio = "programador back-end",
+            //    Image = "https://www.instagram.com/cleberton_dev/",
+            //    PasswordHash = "123321558899"
+            //};
 
-            var category = new Category 
-            { 
-                Name = "Backend", 
-                Slug = "backend" 
-            };
+            //var category = new Category 
+            //{ 
+            //    Name = "Backend", 
+            //    Slug = "backend" 
+            //};
 
-            var post = new Post
-            {
-                Author = user,
-                Category = category,
-                Body = "<p>Hello world</p>",
-                Slug = "comecando-com-ef-core",
-                Summary = "Neste artigo vamos aprender EF core",
-                Title = "Começando com EF Core",
-                CreateDate = DateTime.Now,
-                LastUpdateDate = DateTime.Now,
-            };
+            //var post = new Post
+            //{
+            //    Author = user,
+            //    Category = category,
+            //    Body = "<p>Hello world</p>",
+            //    Slug = "comecando-com-ef-core",
+            //    Summary = "Neste artigo vamos aprender EF core",
+            //    Title = "Começando com EF Core",
+            //    CreateDate = DateTime.Now,
+            //    LastUpdateDate = DateTime.Now,
+            //};
 
-            context.Posts.Add(post);
-            context.SaveChanges();
+            //context.Posts.Add(post);
+            //context.SaveChanges();
+
+            // List
+
+            var posts = context.Posts
+                .AsNoTracking()
+                .OrderByDescending(x => x.LastUpdateDate)
+                .ToList();
+
+            foreach(var post in posts)
+                Console.WriteLine(post.Title);
         }
     }
 }
