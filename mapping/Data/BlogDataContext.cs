5d63e4678fd05c1119bc42ae27c47bbb198b483d
Cleberton Carvalho <clebertonfgc@gmail.com>
2023-02-17 22:26:58 -0300

    Add UserRole, Role, PostTag and Tag

--- mapping/Data/BlogDataContext.cs
+++ mapping/Data/BlogDataContext.cs
@@ -10,10 +10,10 @@ namespace basic_operations.Data
         public DbSet<Post> Posts { get; set; }
         public DbSet<User> Users { get; set; }
 
-        // public DbSet<UserRole> UserRoles { get; set; }
-        // public DbSet<PostTag> PostTags { get; set; }
-        // public DbSet<Role> Roles { get; set; }
-        // public DbSet<Tag> Tags { get; set; }
+        public DbSet<UserRole> UserRoles { get; set; }
+        public DbSet<PostTag> PostTags { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Tag> Tags { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
