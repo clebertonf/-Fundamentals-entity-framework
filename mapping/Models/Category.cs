38d2d7001cd2d9e30a24eedb4ba251e752991b8a
Cleberton Carvalho <clebertonfgc@gmail.com>
2023-02-18 22:10:38 -0300

    Remove dataAnotations

--- mapping/Models/Category.cs
+++ mapping/Models/Category.cs
@@ -3,21 +3,10 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace mapping.Models
 {
-    [Table("Category")]
     public class Category
     {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] // Deixa explicito que o banco irá gera a chave primaria Id
         public int Id { get; set; }
-        [Required]
-        [MinLength(3)]
-        [MaxLength(80)]
-        [Column("Name", TypeName = "NVARCHAR")]
         public string Name { get; set; }
-        [Required]
-        [MinLength(3)]
-        [MaxLength(80)]
-        [Column("Slug", TypeName = "VARCHAR")]
         public string Slug { get; set; }
     }
 }
--- mapping/Models/Post.cs
+++ mapping/Models/Post.cs
@@ -4,11 +4,8 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace mapping.Models
 {
-    [Table("Post")]
     public class Post
     {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string Title { get; set; }
         public string Summary { get; set; }
@@ -16,13 +13,7 @@ namespace mapping.Models
         public string Slug { get; set; }
         public DateTime CreateDate { get; set; }
         public DateTime LastUpdateDate { get; set; }
-
-        [ForeignKey("CategoryId")]
         public int CategoryId { get; set; }
-        public Category Category { get; set; } // propriedade de navegação
-
-        [ForeignKey("AuthorId")]
         public int AuthorId { get; set; }
-        public User Author { get; set; } // propriedade de navegação
     }
 }
