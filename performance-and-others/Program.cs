186a3e7b37d3a3623d2c5bace0b798673329b702
Cleberton Carvalho <clebertonfgc@gmail.com>
2023-02-21 22:18:08 -0300

    Add project performance and others

---
+++ performance-and-others/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace performance_and_others
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
